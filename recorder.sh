62f56cce97d87e71de08e6e01da0a7dd575a6bed
Corey Talbert <59098922+coreytalbert@users.noreply.github.com>
2023-04-14 16:28:42 -0700

    Create recorder.sh

---
+++ recorder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# recorder.sh
+# Corey Talbert
+# April 2023
+#
+# The user must supply frequency as a command line argument.
+
+# File
+timestamp() { date; }
+
+# 160.71M - San Diego Trolly
+$FREQ = $1
+$RTL_MODE = fm
+$GAIN = 50
+$RTL_SAMPLE_RATE = 200k
+$RTL_FM_RESAMPLE_RATE = 12k
+
+echo "Starting scannerbot recorder on frequency $1"
+
+rtl_fm -M $RTL_MODE -s $RTL_SAMPLE_RATE -r $RTL_FM_RESAMPLE_RATE -g $GAIN -f $FREQ \  
+| play -r $RTL_FM_RESAMPLE_RATE -t raw -e signed -b 16 -c 1 -V1 - \
+| rec "$(timestamp)".wav \
