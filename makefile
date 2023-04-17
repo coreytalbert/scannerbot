94ea620553ccc0420ac878ee1906af3a910a3f11
coreytalbert <coreytalbert@gmail.com>
2023-04-16 21:55:33 -0700

    Bus, makefile, and gitignore.

--- makefile
+++ makefile
@@ -1,1 +1,19 @@
-g++ test2.cpp -o test -lrtlsdr
+CXX=g++
+CXXFLAGS=-std=c++20 -g -Wall -Wextra -pedantic -O3 -pthread
+
+# File names
+EXEC 	 = bin/scannerbot
+SOURCES := $(wildcard src/*.cpp)
+OBJECTS := $(SOURCES:.c=.o)
+
+# Target executable
+$(EXEC): $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $(OBJECTS) -o $(EXEC)
+
+# Object files
+obj/%.o: src/%.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+# Clean
+clean:
+	rm -f $(OBJECTS) $(EXEC)
--- src/bus.cpp
+++ src/bus.cpp
@@ -1,47 +1,105 @@
-#include <filesystem>
 #include <chrono>
+#include <exception>
 #include <iostream>
-#include <unordered_set>
+#include <filesystem>
+#include <csignal>
 #include <thread>
+#include <unordered_set>
+#include <vector>
 using std::cout;
-using std::filesystem::directory_iterator;
-using std::filesystem::path;
-using std::filesystem::is_regular_file;
 using std::string;
+using std::this_thread::get_id;
 using std::thread;
 using std::unordered_set;
+using std::vector;
+using std::filesystem::directory_entry;
+using std::filesystem::directory_iterator;
+using std::filesystem::is_regular_file;
+using std::filesystem::path;
+
+const path audio_dir("/home/corey/scannerbot/audio");
+const path transcript_dir("/home/corey/scannerbot/transcripts/");
 
 bool do_exit = false;
+vector<thread *> threads;
 
-void watch_directory(const path& dir_path) {
-    directory_iterator end_itr;
+void signalHandler(int signum)
+{
+    cout << "Interrupt signal (" << signum << ") received.\n";
+    do_exit = true;
 
-    unordered_set<string> seen_files;
+    for (thread * t : threads)
+        if (t->joinable())
+            t->join();
+    
+    exit(signum);
+}
 
-    while (not do_exit) {
-        for (directory_iterator itr(dir_path); itr != end_itr; ++itr) {
-            if (is_regular_file(itr->status())) {
-                string filename = itr->path().filename().string();
-                if (seen_files.find(filename) == seen_files.end()) {
-                    cout << "New file detected: " << itr->path().string() << '\n';
-                    seen_files.insert(filename);
-                    // Do something with the new file
-                }
+void watch_directories()
+{
+    //cout << "\nThread " << std::this_thread::get_id() << " entering watch_directories";
+
+    unordered_set<string> seen_audio_files;
+    unordered_set<string> seen_transcript_files;
+
+    while (not do_exit)
+    {
+        //cout << "\nEntering while loop...";
+        // Handle new audio files.
+        for (auto& file : directory_iterator(audio_dir))
+        {
+            //cout << "\nIterating audio directory...";
+            if (seen_audio_files.count(file.path()) > 0)
+                continue;
+            
+            seen_audio_files.insert(file.path());
+
+            if (is_regular_file(file.status()))
+            {
+                //cout << "\nNew audio detected: " << file.path();  
+                // Send the new audio file to the transcriber.
+                string command = "python3 /home/corey/scannerbot/src/transcriber.py \"";
+                command += file.path();
+                command += "\"";
+                //command += " > /dev/null";
+                int trans_ret_status = system(command.c_str());
+                
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        // Handle new transcripts.
+        for (auto& file : directory_iterator(transcript_dir))
+        {
+            cout << "\nIterating transcript directory...";
+            if (seen_transcript_files.count(file.path()) > 0)
+                continue;
+
+            seen_transcript_files.insert(file.path());
+
+            if (is_regular_file(file.status()))
+            {
+                cout << "\nNew transcript detected: " << file.path();  
+                // Send the new transcript file to the publisher.
+            }
+        }
+
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 }
 
-int main() {
-    path dir_path("path/to/directory");
-
-    thread t(watch_directory, dir_path);
-
-    // Do other stuff in the main thread
-
-    t.join();
-
+int main()
+{
+    try {
+        cout << "\nEntering main...";
+        signal(SIGINT, signalHandler);
+        threads.push_back(new thread(watch_directories));
+        for (thread * t : threads)
+            t->join();
+    }
+    catch (std::exception e) {
+        cout << '\n' <<e.what() ;
+        exit(1);
+    }
     return 0;
 }
