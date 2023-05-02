2180623b389ec39e3d52f4af502203a8ff99f3bf
letriandrew <letriandrew@gmail.com>
2023-05-02 02:00:45 -0700

    basic db

---
+++ src/database.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE recordings(transmissionID INTEGER NOT NULL, partID INTEGER NOT NULL, Timestamp NOT NULL, Duration INTEGER NOT NULL, Freqs INTEGER NOT NULL, Agency NOT NULL, filePath NOT NULL, fileType NOT NULL, audioRes NOT NULL);
+
+-- INSERT INTO recordings(transmissionID, partID, Timestamp, Duration, Freqs, Agency, filePath, fileType, audioRes)
+-- VALUES (1, 1, "00:00", 30, 400, "Test", "Test", "Test", "Test");
+
+SELECT * FROM recordings;
